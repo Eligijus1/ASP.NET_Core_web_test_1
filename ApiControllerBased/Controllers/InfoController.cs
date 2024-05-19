492c146d61a968d0aaddfef87bd33c2d0b99bf8f
Eligijus Stugys <eligijus.stugys@nfq.com>
2024-05-19 10:06:57 +0300

    Added new test controller  ApiControllerBased/Controllers/InfoController.cs

---
+++ ApiControllerBased/Controllers/InfoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiControllerBased.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfoController : ControllerBase {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public InfoController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetAboutInformation")]
+        public string Get()
+        {
+            this._logger.LogInformation("INFO_CONTROLLER_GET_START");
+
+            return "Test API v2024-05-18";
+        }
+    }
+}
