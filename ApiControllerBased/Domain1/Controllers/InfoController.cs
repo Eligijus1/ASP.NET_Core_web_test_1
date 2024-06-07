a13026756c19a5690e5ce3b97a5afbcd1fc01788
Eligijus Stugys <eligijus.stugys@nfq.com>
2024-06-07 16:50:59 +0300

    Latest changes

--- ApiControllerBased/Domain1/Controllers/InfoController.cs
+++ ApiControllerBased/Domain1/Controllers/InfoController.cs
@@ -8,23 +8,27 @@ namespace ApiControllerBased.Domain1.Controllers
     public class InfoController : ControllerBase
     {
         private readonly ILogger<InfoController> _logger;
+        private readonly IConfiguration Configuration;
 
-        public InfoController(ILogger<InfoController> logger)
+        public InfoController(ILogger<InfoController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         [HttpGet(Name = "GetAboutInformation")]
         public string Get()
         {
-            _logger.LogInformation("{Date} {Time} UTC INFO_CONTROLLER_GET_START. Process ID: {ProcessId}. Used {RAM} bytes of RAM.",
+            _logger.LogInformation("{Date} {Time} UTC INFO_CONTROLLER_GET_START. Process ID: {ProcessId}. Used {RAM} bytes of RAM. Environment version is {Version}. MyKey1 value is '{MyKey1}'.",
                 DateTime.UtcNow.ToShortDateString(),
                 DateTime.UtcNow.ToLongTimeString(),
                 Process.GetCurrentProcess().Id,
-                GC.GetTotalMemory(true)
+                GC.GetTotalMemory(true),
+                Environment.Version.ToString(),
+                Configuration["MyKey1"]
             );
 
-            return "Test API v2024-06-01";
+            return "Test API v2024-06-07";
         }
     }
 }
--- ApiControllerBased/Program.cs
+++ ApiControllerBased/Program.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddRazorPages();
 
+// Add services to the container.
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,16 +14,45 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// Redirect in production HTTP requests to HTTPS:
+/*
+if (app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
+*/
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapRazorPages();
+
+app.Logger.LogInformation("IsDevelopment={IsDevelopment}; IsProduction={IsProduction}; IsStaging={IsStaging}; EnvironmentName={EnvironmentName}; DebuggerIsAttached={DebuggerIsAttached};"
+    , [
+        app.Environment.IsDevelopment(),
+        app.Environment.IsProduction(),
+        app.Environment.IsStaging(),
+        app.Environment.EnvironmentName,
+        Debugger.IsAttached
+      ]
+    );
+
+// Add what "/" should return. 
+// Add app.MapGet("/", () => "Hello World!");
+
 app.Run();
+
+// DEBUG URL:
+// http://localhost/swagger/index.html
+// http://localhost/swagger/v1/swagger.json
